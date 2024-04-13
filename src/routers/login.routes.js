b7d1a1927c704abd8344ee032d617a0d356da113
Felipe-33 <fepepiuerj@gmail.com>
2024-04-13 01:31:31 -0300

    Requisito 4

--- src/routers/login.routes.js
+++ src/routers/login.routes.js
@@ -3,8 +3,30 @@ const crypto = require('crypto');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-  // const { email, password } = req.body;
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const emailRegex = /\S+@\S+\.\S+/;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
+router.post('/login', validateEmail, validatePassword, (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(200).json({ token });
 });
